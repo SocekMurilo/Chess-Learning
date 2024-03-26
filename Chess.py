7773ed029e7b93cf2fb28e28698e38331b2a2b6b
Murilo Kauan Socek <socekmurilo@gmail.com>
2024-03-26 16:50:23 -0300

    init clustering

--- Chess.py
+++ Chess.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from joblib import dump
 from xgboost import XGBRegressor
+from sklearn.cluster import MiniBatchKMeans
+
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import normalize
@@ -20,65 +22,67 @@ df.drop(
     axis = 1,
     inplace = True
 )
+
+
+# print(df)
+
+top10 = []
 le = LabelEncoder()
-print("Comecou")
-moves = df["moves"].str.split()
-moves = moves.T
-lenthMoves = len(moves)
+
+print("Comecou!!")
+
+# moves = moves.T
+df['moves'] = df["moves"].str.split()
+moves = df['moves']
+i = 0
+
 for sublist in moves:
+    moves = sublist[0: 10: ]
     for item in sublist:
-        if item in df.columns:
-            df.loc[len(df)] = df[item]
-        else:
-            df[item] = item
+        if item in df.loc[i, 'moves']:
+            df.loc[i, item] = (int)(1)
+    i += 1
 
-        # df[item] = le.fit_transform(df[item])
-print(moves)
-print(df.columns)
-print (df)
-# for col in moves:
-#     features = 
-# print(features)
+df = df.fillna((int)(0))
 
-
-        # if item not in df.columns:
-        #     df[item] = item
-        # else:
-        #     if item in df[item].values:
-        #         df.loc[len(df), item] = item
-        # print(item)
-
-
-
+df.drop(
+    [ 'moves'],
+    axis = 1,
+    inplace = True
+)
+print(df)
+print(df["e4"])
 #######################################################################################
 
-# le = LabelEncoder()
-# df['victory_status'] = le.fit_transform(df['victory_status'])
-# df['winner'] = le.fit_transform(df['winner'])
-# df['increment_code'] = le.fit_transform(df['increment_code'])
+df['victory_status'] = le.fit_transform(df['victory_status'])
+df['winner'] = le.fit_transform(df['winner'])
+df['increment_code'] = le.fit_transform(df['increment_code'])
 # df['moves'] = le.fit_transform(df['moves'])
-# df['opening_eco'] = le.fit_transform(df['opening_eco'])
-# df['opening_name'] = le.fit_transform(df['opening_name'])
+df['opening_eco'] = le.fit_transform(df['opening_eco'])
+df['opening_name'] = le.fit_transform(df['opening_name'])
 
-# Y = df['white_rating']
-# X = df.drop(['black_rating', 'white_rating'], axis = 1)
-# X = normalize(X)
+Y = df['winner']
+X = df.drop(['winner'], axis = 1)
+X = normalize(X)
 
-# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.30, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.30, random_state=42)
 
-# model = RandomForestClassifier()
+model = MiniBatchKMeans(n_clusters=10,
+                        random_state=0,
+                        batch_size=6,
+                        n_init="auto", )
 
-# model.fit(X_train, Y_train)
+model.fit(X_train, Y_train)
 
-# dump(model, 'chess.pkl')
+dump(model, 'chess.csv')
 
-# print("-----------------------------------------------")
-# print(mean_absolute_error(Y_train, model.predict(X_train)))
-# print(mean_absolute_error(Y_test, model.predict(X_test)))
-# print("-----------------------------------------------")
+print("-----------------------------------------------")
+print(mean_absolute_error(Y_train, model.predict(X_train)))
+print(mean_absolute_error(Y_test, model.predict(X_test)))
+print("-----------------------------------------------")
 
 
-# Ypred = model.predict(X)
-# plt.plot(Y)
-# plt.plot(Ypred)
-# plt.show()
+Ypred = model.fit_predict(X)
+plt.plot(Y)
+plt.plot(Ypred)
+plt.show()
