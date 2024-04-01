6827394bde1e456870982f8513060e7eccc9bc51
Murilo Kauan Socek <socekmurilo@gmail.com>
2024-04-01 01:41:21 -0300

    Chess Clustering

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import pandas as pd
+from joblib import load
+from sklearn.calibration import LabelEncoder
 
 app = Flask(__name__)
 
+model = load("chess.pkl")
+
 @app.route("/")
 def home():
-    return "Hello, World!"
-    
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
